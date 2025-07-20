30a47df19ec087cae8e3823527ce9eb5a6426e7e
Alvaro Serrano Andres <aserranoandres23@gmail.com>
2025-07-20 13:52:06 +0200

    Integration tests & updated README

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # PriceService
-API REST para obtener precios de productos
+
+API REST para obtener precios de productos.
+
+## Descripción
+
+PriceService es una API REST que permite consultar el precio aplicable de un producto en función de la fecha, el identificador del producto y el identificador de la marca. La API devuelve el precio con la mayor prioridad dentro del rango de fechas especificado.
+
+## Características
+
+- Consulta de precios por fecha, producto y marca.
+- Manejo de prioridades para determinar el precio aplicable.
+- Respuesta en formato JSON con detalles del precio.
+
+## Tecnologías utilizadas
+
+- **Java 17**
+- **Spring Boot**
+- **Maven**
+- **H2 Database** (base de datos en memoria para pruebas)
+- **JPA/Hibernate** (para el acceso a datos)
+- **Lombok** (para reducir código repetitivo)
+
+## Requisitos previos
+
+- Java 17 o superior
+- Maven 3.8 o superior
+
+## Configuración y ejecución
+
+1. Clona este repositorio:
+   ```bash
+   git clone https://github.com/x357496-sgt_sangroup/PriceService.git
+   cd PriceService
+   
+2. Compila el proyecto:
+    ```bash
+    mvn clean install
+   
+3. Ejecuta la aplicación:
+    ```bash
+    mvn spring-boot:run
+
+4. Accede a la API en `http://localhost:8081/api/prices`.
+5. Realiza peticiones GET a la API con los parámetros necesarios:
+    - `productId`: ID del producto.
+    - `brandId`: ID de la marca.
+    - `date`: Fecha en formato `yyyy-MM-dd HH:mm:ss`.
+
+## Ejemplo de petición
+
+```http
+curl -X GET "http://localhost:8081/api/prices?applicationDate=2020-06-14T10:00:00&productId=35455&brandId=1"
---
+++ src/test/java/com/alvaroserrano/price_service/infraestructure/controller/PriceControllerIntegrationTest.java
@@ -0,0 +1,88 @@
+package com.alvaroserrano.price_service.infraestructure.controller;
+
+import com.alvaroserrano.price_service.PriceServiceApplication;
+import com.alvaroserrano.price_service.domain.port.PriceRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(
+        classes = PriceServiceApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@AutoConfigureMockMvc
+class PriceControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PriceRepository priceRepository;
+
+    @Test
+    @DisplayName("Test 1: 14/06/2020 10:00 - product 35455 - brand 1")
+    void test1() throws Exception {
+        mockMvc.perform(get("/api/prices")
+                        .param("applicationDate", "2020-06-14T10:00:00")
+                        .param("productId", "35455")
+                        .param("brandId", "1")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(35.50));
+    }
+
+    @Test
+    @DisplayName("Test 2: 14/06/2020 16:00 - product 35455 - brand 1")
+    void test2() throws Exception {
+        mockMvc.perform(get("/api/prices")
+                        .param("applicationDate", "2020-06-14T16:00:00")
+                        .param("productId", "35455")
+                        .param("brandId", "1")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(25.45));
+    }
+
+    @Test
+    @DisplayName("Test 3: 14/06/2020 21:00 - product 35455 - brand 1")
+    void test3() throws Exception {
+        mockMvc.perform(get("/api/prices")
+                        .param("applicationDate", "2020-06-14T21:00:00")
+                        .param("productId", "35455")
+                        .param("brandId", "1")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(35.50));
+    }
+
+    @Test
+    @DisplayName("Test 4: 15/06/2020 10:00 - product 35455 - brand 1")
+    void test4() throws Exception {
+        mockMvc.perform(get("/api/prices")
+                        .param("applicationDate", "2020-06-15T10:00:00")
+                        .param("productId", "35455")
+                        .param("brandId", "1")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(30.50));
+    }
+
+    @Test
+    @DisplayName("Test 5: 16/06/2020 21:00 - product 35455 - brand 1")
+    void test5() throws Exception {
+        mockMvc.perform(get("/api/prices")
+                        .param("applicationDate", "2020-06-16T21:00:00")
+                        .param("productId", "35455")
+                        .param("brandId", "1")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(38.95));
+    }
+}
